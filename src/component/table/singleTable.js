b52a72433e32e39acf88dd9270e8860de38f7b8e
gouta01mehta <goutammehta@gmail.com>
2023-03-16 18:16:50 +0530

    table styling & component structure of singleTable

--- src/component/table/singleTable.js
+++ src/component/table/singleTable.js
@@ -1,16 +1,143 @@
-
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from "prop-types";
-import MainTable from '../../table/mainTable';
-export default function SingleTable() {
+import { Box, TextField, Tab,Button, ClickAwayListener} from '@mui/material';
+import Dropdown from '../dropdown';
+import {updateTable, deleteTable } from '../../api/tableApi';
+import { bulkAddColumns } from '../../store/table/tableThunk';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export default function SingleTable({dbData,table,setTabIndex,getAllTableName,index,tabIndex,highlightActiveTable}) {
+  const navigate = useNavigate();
   
+  const [tableNa, setTableNa] = useState(null);
+  const [, setTableButton] = useState(false);
+  const [name,setName] = useState();
+
+  const dispatch= useDispatch();
+ 
+
+  const TabWithDropdown = ({ label, dropdown }) => (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Tab label={label} />
+      {dropdown}
+    </Box>
+  );
+
+  const renameTableName = async (db_id, tableName) => {
+    const data1 = {
+      newTableName: tableNa || table[0]
+      
+    };
+    await updateTable(db_id,tableName, data1);
+    await getAllTableName(dbData?.db?._id, dbData?.db?.org_id?._id);
+    setTableNa(null);
+  };
+  const deleteTableName = async (tableid) => {
+    // console.log("deltetb",tableid);
+    await deleteTable(dbData?.db?._id, tableid);
+    await getAllTableName(dbData?.db?._id, dbData?.db?.org_id?._id);
+  };
+  function onTableClicked() {
+    navigate(`/db/${dbData?.db?._id}/table/${table[0]}`);
+    dispatch(bulkAddColumns({
+      "dbId":dbData?.db?._id,
+      "tableName": table[0]
+    }));
+    setTableButton(true);
+    highlightActiveTable()
+  }
+
   return (
     <> 
-      <MainTable/>
+    <Box
+              sx={{
+                p: '5px',
+                borderRadius: '8px',
+                border: '1px solid rgba(0, 0, 0, 0.2)',
+                backgroundColor: '#fff',
+                boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+                // minWidth: '175px',
+                // maxWidth: '200px',
+                width: "135px",
+                textAlign: 'center',
+                //textOverflow: 'ellipsis',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                transition: 'transform 0.2s ease-in-out',
+              }}
+              
+              onClick={() =>{ onTableClicked(table[0])
+                dispatch(bulkAddColumns({
+                  "dbId":dbData?.db?._id,
+                  "tableName": table[0]
+                }));
+              } }
+            >
+              {name && tabIndex == index?
+              
+                  (<>
+                   <ClickAwayListener onClickAway={()=>{setName(false)}} >
+                   <Box>
+                    <TextField
+                      // onBlur={handleOpen}
+                      defaultValue ={table[1]?.tableName || table[0]}
+                      autoFocus sx={{ width: 75, fontWeight: 'bold' }} value={tableNa  }
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          renameTableName(dbData?.db?._id,table[0]);
+                          setName(false)
+                        }
+                      }}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                       
+                      }}
+                      onChange={(e) => { setTableNa(e.target.value) }} size="small" />
+                    <Button
+                      sx={{
+                        width: 2,
+                        fontSize: 8,
+                      }}
+                      type='submit' onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        setName(false)
+                        renameTableName(dbData?.db?._id, table[0])
+                      }}
+                      variant="contained" >
+                      Rename
+                    </Button>
+                    </Box>
+                      </ClickAwayListener>
+                  </>) :
+                  (<>
+                    < Box sx={{ mt: -1 }}>
+                      <TabWithDropdown sx={{width: 100}}
+                       label={table[1]?.tableName || table[0]}
+                        dropdown={<Dropdown  setTabIndex={setTabIndex}
+                          tableId={table[0]} title={table[1]?.tableName || table[0]} tabIndex={index}
+                        first="Rename" second="Delete" idToDelete={dbData?.db?._id}
+                          deleteFunction={deleteTableName} setName={setName} />}
+                      />
+                    </Box>
+                  </>)
+                }
+              </Box>
     </>
+    
   )
 }
 SingleTable.propTypes = {
+  dbData: PropTypes.any,
   table: PropTypes.string,
-  dbId: PropTypes.string
+  dbId: PropTypes.string,
+  label : PropTypes.any,
+  dropdown:PropTypes.any,
+  getAllTableName:PropTypes.func,
+  highlightActiveTable:PropTypes.func,
+  index:PropTypes.number,
+  tabIndex:PropTypes.number,
+  setTabIndex:PropTypes.func
 };
--- src/component/table/tablesList.js
+++ src/component/table/tablesList.js
@@ -2,48 +2,29 @@ import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import PopupModal from '../popupModal';
-import { createTable, updateTable, deleteTable } from '../../api/tableApi';
+import { createTable } from '../../api/tableApi';
 import { getDbById } from '../../api/dbApi';
 import PropTypes from "prop-types";
 import SingleTable from './singleTable';
 import {useNavigate} from "react-router-dom";
 import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Dropdown from '../dropdown';
 import { bulkAddColumns } from '../../store/table/tableThunk';
 import { useDispatch } from 'react-redux';
-import { TextField } from '@mui/material';
+import MainTable from '../../table/mainTable';
 
 export default function TablesList({dbData,tables,setTables}) {
-  // const [tables, setTables] = useState(0);
-  const [tabIndex, setTabIndex] = useState(-1);
-  const [tableNa, setTableNa] = useState(null);
-  const [name,setName] = useState();
   const dispatch= useDispatch();
-  const [value, setValue] = React.useState(0);
-  
-  const navigate = useNavigate();
+  const [value, setValue] = React.useState(0);  
+  const navigate =useNavigate();
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    console.log("new",newValue)
   };
-  // state to display modal
-  const TabWithDropdown = ({ label, dropdown }) => (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Tab label={label} />
-      {dropdown}
-    </Box>
-  );
   const [table, setTable] = useState();
-  const [, setTableButton] = useState(false);
-  const [clickedTable, setClickedTable] = useState('');
+  const [tabIndex,setTabIndex]= useState(-1);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
-  // Object.entries(tables)[0][0]
-  function onTableClicked(value) {
-    navigate(`/db/${dbData?.db?._id}/table/${value}`);
-    setClickedTable(value);
-    setTableButton(true);
-  }
+  
   const saveTable = async () => {
     const dbId = dbData?.db._id;
     const data = {
@@ -60,7 +41,7 @@ export default function TablesList({dbData,tables,setTables}) {
   }, [dbData]);
   useEffect(() => {
     if (Object.entries(tables)?.length >0){
-      onTableClicked(Object.keys(tables)[0])
+      navigate(`/db/${dbData?.db?._id}/table/${Object.keys(tables)[0]}`);
       dispatch(bulkAddColumns({
         "dbId":dbData?.db?._id,
         "tableName": Object.keys(tables)[0]
@@ -72,24 +53,11 @@ export default function TablesList({dbData,tables,setTables}) {
     setTables(data.data.data.tables || {});
     return data;
   }
-  const renameTableName = async (db_id, tableName) => {
-    const data1 = {
-      newTableName: tableNa || table[0]
-    };
-    await updateTable(db_id,tableName, data1);
-    await getAllTableName(dbData?.db?._id, dbData?.db?.org_id?._id);
-    setTableNa(null);
-  };
-  const deleteTableName = async (tableid) => {
-    // console.log("deltetb",tableid);
-    await deleteTable(dbData?.db?._id, tableid);
-    await getAllTableName(dbData?.db?._id, dbData?.db?.org_id?._id);
-  };
   
   return (
     <>
-      <Box sx={{ width: "100%", display: "flex", height: "33px" }}>
-        <Box  sx={{ display: 'flex', overflow: 'hidden', width: "100%", height: "47px"}}>
+      <Box sx={{ width: "100%", display: "flex", height: "auto" }}>
+        <Box  sx={{ display: 'flex', overflow: 'hidden', width: "100%", height: "auto"}} >
         <Tabs
         value={value}
         onChange={handleChange}
@@ -98,100 +66,18 @@ export default function TablesList({dbData,tables,setTables}) {
         aria-label="scrollable auto tabs example"
       >
           {Object.entries(tables).map((table, index) => (
-            <Box
-              key={index}
-              sx={{
-                p: '5px',
-                borderRadius: '8px',
-                border: '1px solid rgba(0, 0, 0, 0.2)',
-                backgroundColor: '#fff',
-                boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-                minWidth: '175px',
-                maxWidth: '200px',
-                textAlign: 'center',
-                textOverflow: 'ellipsis',
-                overflow: 'hidden',
-                whiteSpace: 'nowrap',
-                transition: 'transform 0.2s ease-in-out',
-              }}
-              
-              onClick={() =>{ onTableClicked(table[0]  )
-                dispatch(bulkAddColumns({
-                  "dbId":dbData?.db?._id,
-                  "tableName": table[0]
-                }));
-              } }
-            >
-
-              {name && tabIndex == index ?
-                  (<>
-                    <TextField
-                      // onBlur={handleOpen}
-                      defaultValue ={table[1]?.tableName || table[0]}
-                      autoFocus sx={{ width: 75, fontWeight: 'bold' }} value={tableNa  }
-                      onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                          renameTableName(dbData?.db?._id,table[0]);
-                          setTabIndex(-1);
-                          setName(false)
-                        }
-                      }}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                       
-                      }}
-                      onChange={(e) => { setTableNa(e.target.value) }} size="small" />
-                    <Button
-                      sx={{
-                        width: 2,
-                        fontSize: 8,
-                      }}
-                      type='submit' onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        setTabIndex(-1);
-                        setName(false)
-                        renameTableName(dbData?.db?._id, table[0])
-                      }}
-                      variant="contained" >
-                      Rename
-                    </Button>
-                  </>) :
-                  (<>
-                    < Box sx={{ mt: -1 }}>
-                      <TabWithDropdown
-                       label={table[1]?.tableName || table[0]}
-                        dropdown={<Dropdown tableId={table[0]} title={table[1]?.tableName || table[0]} 
-                        tabIndex={index} setTabIndex={setTabIndex} 
-                        first="Rename" second="Delete" idToDelete={dbData?.db?._id}
-                          deleteFunction={deleteTableName} setName={setName} />}
-                      />
-                    </Box>
-                  </>)
-                }
-              </Box>
-            ))}
+            <Box key={index} >
+              <SingleTable table={table} tabIndex={tabIndex}  setTabIndex={setTabIndex} getAllTableName={getAllTableName} index={index} dbData={dbData} highlightActiveTable={()=>setValue(index)}/>
+            </Box>
+            ))
+          }
           </Tabs>
         </Box>
         <Button onClick={() => handleOpen()} variant="contained" sx={{ width: 122 }} >
           Add Table
         </Button> </Box>
-              {/* <TabWithDropdown
-      label={table[1]?.tableName || table[0]}
-      dropdown={<Dropdown />}
-    />
-            </Box>
-          ))}
-          </Tabs>
-        </Box>
-        <Button onClick={handleOpen} variant="contained" sx={{width:122}} >
-          Add Table
-        </Button> </Box> */}
         <PopupModal title="create table" label="Table Name" open={open} setOpen={setOpen} submitData={saveTable} setVariable={setTable} />
-      <Box>
-          <SingleTable  table={clickedTable} />
-      </Box>
+        <MainTable/>
     </>
   );
 }
