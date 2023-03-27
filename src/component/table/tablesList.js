fbd27bcc7555e4172428d4b93bbbfc63a2100046
apple <apple@apples-iMac.local>
2023-03-27 17:15:00 +0530

    filter dropdown downloaded

--- src/component/table/tablesList.js
+++ src/component/table/tablesList.js
@@ -5,51 +5,61 @@ import PopupModal from '../popupModal';
 import FilterModal from "../filterPopUp"
 import PropTypes from "prop-types";
 import SingleTable from './singleTable';
-import {useNavigate,useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Tabs from '@mui/material/Tabs';
 import { bulkAddColumns } from '../../store/table/tableThunk';
 import { useDispatch, useSelector } from 'react-redux';
 import MainTable from '../../table/mainTable';
 import { getAllTableInfo } from '../../store/allTable/allTableSelector';
-import { createTable1} from '../../store/allTable/allTableThunk';
-import EditIcon from '@mui/icons-material/Edit';
-export default function TablesList({dbData}) {
-  const dispatch= useDispatch();
+import { createTable1 } from '../../store/allTable/allTableThunk';
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+export default function TablesList({ dbData }) {
+  const dispatch = useDispatch();
   const params = useParams();
 
   const AllTableInfo = useSelector((state) => getAllTableInfo(state));
 
 
-  const [value, setValue] = useState(0);  
-  const navigate =useNavigate();
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
   const handleChange = (event, newValue) => {
     setValue(newValue);
-    // const tableNames = Object.keys(AllTableInfo.tables);
-    // const selectedTableName = tableNames[newValue];
-    // navigate(`/db/${dbData?.db?._id}/table/${selectedTableName}`);
   };
   const [table, setTable] = useState();
-  const [tabIndex,setTabIndex]= useState(-1);
+  const [tabIndex, setTabIndex] = useState(-1);
   const [open, setOpen] = useState(false);
-  const [openn,setOpenn] = useState(false)
-  const [filter,setFilter]=useState(false);
+  const [openn, setOpenn] = useState(false)
+  const [filter, setFilter] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleOpenn = () => setOpenn(true);
-  const [edit,setEdit] = useState(false)
-  const [filterId,setFilterId] = useState("")
+  const [edit, setEdit] = useState(false)
+  const [filterId, setFilterId] = useState("")
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  console.log(value)
   const saveTable = async () => {
     const data = {
       tableName: table
     }
     setOpen(false);
-    dispatch(createTable1({"dbId":dbData?.db?._id,"data":data}));
- 
+    dispatch(createTable1({ "dbId": dbData?.db?._id, "data": data }));
+
   };
-  const handleEdit = async()=>{
+  const handleEdit = async () => {
     setEdit(true)
     setOpenn(true)
   }
-  function onFilterClicked(filter,id) {
+  function onFilterClicked(filter, id) {
     setFilterId(id)
     dispatch(bulkAddColumns({
       "dbId": dbData?.db?._id,
@@ -57,78 +67,83 @@ export default function TablesList({dbData}) {
       "filter": filter
     }));
   }
-  useEffect(()=>{
-    if(dbData?.db?.tables)
-    {
-      const tableNames = Object.keys(dbData.db.tables);
-    setValue(tableNames?.indexOf(params?.tableName) || 0 );
-    }
-  },[value]);
   useEffect(() => {
-    if(dbData?.db?.tables)
-    {
+    if (dbData?.db?.tables) {
       const tableNames = Object.keys(dbData.db.tables);
-        dispatch(bulkAddColumns({
-          "dbId":dbData?.db?._id,
-          "tableName": params?.tableName|| tableNames[0]
-        }));
-        if(!(params?.tableName))
-        navigate(`/db/${dbData?.db?._id}/table/${tableNames[0]}`);   
-       
+      setValue(tableNames?.indexOf(params?.tableName) || 0);
+    }
+  }, [value]);
+  useEffect(() => {
+    if (dbData?.db?.tables) {
+      const tableNames = Object.keys(dbData.db.tables);
+      dispatch(bulkAddColumns({
+        "dbId": dbData?.db?._id,
+        "tableName": params?.tableName || tableNames[0]
+      }));
+      if (!(params?.tableName))
+        navigate(`/db/${dbData?.db?._id}/table/${tableNames[0]}`);
+      setValue(tableNames?.indexOf(params?.tableName) || 0);
     }
   }, [dbData])
 
   return (
     <>
       <Box sx={{ width: "100%", display: "flex", height: "auto" }}>
-        <Box  sx={{ display: 'flex', overflow: 'hidden', width: "100%", height: "auto"}} >
-        <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-      >
-
-          {AllTableInfo.tables && Object.entries(AllTableInfo.tables).map((table, index) => (
-            <Box key={index} >
-              <SingleTable filter={filter} setFilter = {setFilter} table={table}  tabIndex={tabIndex} setTabIndex={setTabIndex}  index={index} dbData={dbData} highlightActiveTable={()=>setValue(index)}/>
-            </Box>
+        <Box sx={{ display: 'flex', overflow: 'hidden', width: "100%", height: "auto" }} >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="auto"
+            aria-label="scrollable auto tabs example"
+          >
+            {AllTableInfo.tables && Object.entries(AllTableInfo.tables).map((table, index) => (
+              <Box key={index} >
+                <SingleTable filter={filter} setFilter={setFilter} table={table} tabIndex={tabIndex} setTabIndex={setTabIndex} index={index} dbData={dbData} highlightActiveTable={() => setValue(index)} />
+              </Box>
             ))
-          }
+            }
           </Tabs>
         </Box>
         <Button onClick={() => handleOpen()} variant="contained" sx={{ width: 122 }} >
           Add Table
-        </Button>                 
-        </Box>
-        <Box display="flex" flexWrap="nowrap">
-        {filter  &&
+        </Button>
+      </Box>
+      <Box display="flex" flexWrap="nowrap">
+        {filter &&
           Object.entries(filter).map((filter, index) => (
             <Box key={index} marginRight={1}>
-              <Box sx={{backgroundColor:"grey",height:30,width:120,display:"flex", gap:"10px", alignItems:"center",justifyContent:"center"}}
+              <Box sx={{ backgroundColor: "grey", height: 30, width: 120, display: "flex", gap: "10px", alignItems: "center", justifyContent: "center" }}
                 onClick={() => {
-                  onFilterClicked(filter[1].query,filter[0]);
+                  onFilterClicked(filter[1].query, filter[0]);
                 }}
                 variant="contained"
                 color="primary"
               >
                 {filter[1]?.filterName}
-              <EditIcon fontSize='small' onClick={()=>{
-                handleEdit();
-              }}/>
+                <IconButton onClick={handleClick}>
+                  <MoreVertIcon />
+                </IconButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={() => { handleEdit(); }}>Edit</MenuItem>
+                  <MenuItem onClick={handleClose}>Delete</MenuItem>
+                </Menu>
               </Box>
             </Box>
           ))}
 
         <Button onClick={() => handleOpenn()} variant="contained" sx={{ width: 122 }} >
           addFilter
-        </Button> 
+        </Button>
       </Box>
-        <PopupModal title="create table" label="Table Name" open={open} setOpen={setOpen} submitData={saveTable} setVariable={setTable} />
-       { openn && <FilterModal open={openn} edit={edit} setOpen={setOpenn} filterId={filterId} dbId={dbData?.db?._id} tableName={params?.tableName} AllTableInfo ={AllTableInfo}/>}
-        
-        <MainTable/>
+      <PopupModal title="create table" label="Table Name" open={open} setOpen={setOpen} submitData={saveTable} setVariable={setTable} />
+      {openn && <FilterModal open={openn} edit={edit} setOpen={setOpenn} filterId={filterId} dbId={dbData?.db?._id} tableName={params?.tableName} AllTableInfo={AllTableInfo} />}
+
+      <MainTable />
     </>
   );
 }
@@ -137,8 +152,8 @@ TablesList.propTypes = {
   table: PropTypes.string,
   dbId: PropTypes.string,
   orgId: PropTypes.string,
-  tables:PropTypes.any,
-  dropdown:PropTypes.any,
-  label : PropTypes.any,
-  setTables:PropTypes.any
+  tables: PropTypes.any,
+  dropdown: PropTypes.any,
+  label: PropTypes.any,
+  setTables: PropTypes.any
 };
