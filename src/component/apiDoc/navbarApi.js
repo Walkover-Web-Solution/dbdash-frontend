fed6fd8317aa1504449198678987e6822917f79c
Deepak solanki <deepak.solanki2335@gmail.com>
2023-05-19 11:33:56 +0530

    arrage the org ascending

--- src/component/apiDoc/navbarApi.js
+++ src/component/apiDoc/navbarApi.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Link,useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useSelector } from 'react-redux';
-import { Box,Button, Select, MenuItem, FormControl, InputLabel, ListSubheader } from "@mui/material";
+import { Box, Button, Select, MenuItem, FormControl, InputLabel, ListSubheader } from "@mui/material";
 import ApiCrudTablist from './apiCrudTab/apiCrudTablist';
 import { getDbById } from '../../api/dbApi';
 import PropTypes from "prop-types";
@@ -15,7 +15,7 @@ export default function Navbar() {
   const [selectedDb, setSelectedDb] = useState(false);
   const [selectTable, setSelectTable] = useState(false);
   const alldb = useSelector((state) => selectOrgandDb(state))
-  const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const handleChange = async (event) => {
     setSelectedDb(event.target.value);
     setSelectedOption(event.target.value);
@@ -28,10 +28,10 @@ export default function Navbar() {
     setSelectTable(event.target.value);
   };
   const filterDbsBasedOnOrg = async () => {
-    Object.keys(alldb).forEach(async(orgId) =>{
-      const dbObj = alldb[orgId].find(db=>db?._id === params.dbId)
-      if(dbObj){
-       
+    Object.keys(alldb).forEach(async (orgId) => {
+      const dbObj = alldb[orgId].find(db => db?._id === params.dbId)
+      if (dbObj) {
+
         setSelectedOption(dbObj._id);
         setSelectedDb(dbObj._id)
         setDbId(dbObj._id)
@@ -40,7 +40,7 @@ export default function Navbar() {
       }
     })
   }
- 
+
   useEffect(() => {
     filterDbsBasedOnOrg();
   }, [alldb])
@@ -57,47 +57,47 @@ export default function Navbar() {
   return (
     <>
       <Box align="center">
-       
-        
+
+
       </Box>
-      <Box sx={{display:"flex",flexDirection:"row"}}>
-      <Box  sx={{display:"flex",flexDirection:"row"}}>
-      {alldb && selectedDb && <FormControl sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel htmlFor="grouped-select">Organization-db</InputLabel>
-          <Select id="grouped-select" label="Organization and dbs" value={selectedDb} onChange={handleChange}>
-          {Object.entries(alldb)
-  .sort(([, dbs1], [, dbs2]) => dbs1[0].org_id.name.localeCompare(dbs2[0].org_id.name))
-  .map(([orgId, dbs]) => [
-    <ListSubheader key={`${orgId}-header`} name={orgId}>
-      {dbs[0].org_id.name}
-    </ListSubheader>,
-    dbs?.map((db, index) => (
-      <MenuItem key={index} value={db?._id}>
-        {db?.name}
-      </MenuItem>
-    ))
-  ])}
-            {/* defaultValue={selectedOption} */}
-          </Select>
-        </FormControl>}
-        
-      </Box>
-      
-      {Object.keys(tables).length >= 1 && <Box >
-        <FormControl sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel htmlFor="grouped-select">Tables-Name</InputLabel>
-          <Select value={selectTable} label= "Tables-Name"
-            onChange={handleChangeTable} >
-            {Object.entries(tables)?.map((table) => (
-              <MenuItem key={table[0]} value={table[0]} >
-                {table[1].tableName || table[0]}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Box>}
-      {Object.keys(tables).length >= 1 && <Box sx={{ display: 'flex',  alignItems: 'center',marginRight:"10px",position:"fixed",right:0,top:"10vh"}}>
-          
+      <Box sx={{ display: "flex", flexDirection: "row" }}>
+        <Box sx={{ display: "flex", flexDirection: "row" }}>
+          {alldb && selectedDb && <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel htmlFor="grouped-select">Organization-db</InputLabel>
+            <Select id="grouped-select" label="Organization and dbs" value={selectedDb} onChange={handleChange}>
+              {Object.entries(alldb)
+                .sort(([, dbs1], [, dbs2]) => dbs1[0].org_id.name.localeCompare(dbs2[0].org_id.name))
+                .map(([orgId, dbs]) => [
+                  <ListSubheader key={`${orgId}-header`} name={orgId}>
+                    {dbs[0].org_id.name}
+                  </ListSubheader>,
+                  dbs?.map((db, index) => (
+                    <MenuItem key={index} value={db?._id}>
+                      {db?.name}
+                    </MenuItem>
+                  ))
+                ])}
+              {/* defaultValue={selectedOption} */}
+            </Select>
+          </FormControl>}
+
+        </Box>
+
+        {Object.keys(tables).length >= 1 && <Box >
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel htmlFor="grouped-select">Tables-Name</InputLabel>
+            <Select value={selectTable} label="Tables-Name"
+              onChange={handleChangeTable} >
+              {Object.entries(tables)?.map((table) => (
+                <MenuItem key={table[0]} value={table[0]} >
+                  {table[1].tableName || table[0]}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>}
+        {Object.keys(tables).length >= 1 && <Box sx={{ display: 'flex', alignItems: 'center', marginRight: "10px", position: "fixed", right: 0, top: "10vh" }}>
+
           <Link to={`/authkeypage/${dbId}`} state={selectedOption} style={{ textDecoration: 'none' }}>
             <Button variant="contained" color="primary">Auth Key</Button>
           </Link>
@@ -105,7 +105,7 @@ export default function Navbar() {
       </Box>
       <Box>
 
-       {loading &&  <ApiCrudTablist dbId={dbId} db={selectedOption} table={selectTable} />}
+        {loading && <ApiCrudTablist dbId={dbId} db={selectedOption} table={selectTable} />}
       </Box>
     </>
   )
