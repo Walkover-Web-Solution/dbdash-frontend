7f7e306aded62b70e1eb9ea10cf0ba3acf044bfb
Deepak solanki <deepak.solanki2335@gmail.com>
2023-05-20 16:30:40 +0530

    Task complete to arrang the db

--- src/component/workspaceDatabase/workspaceCombined.js
+++ src/component/workspaceDatabase/workspaceCombined.js
@@ -1,22 +1,23 @@
-import React, {  useState } from 'react'
-import { Box } from '@mui/material'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 import PopupModal from '../popupModal';
 import Button from '@mui/material/Button';
 import { OrgList } from './orgList';
 import { PropTypes } from 'prop-types';
-import { selectOrgandDb } from "../../store/database/databaseSelector.js"
+import { selectOrgandDb } from "../../store/database/databaseSelector.js";
 import { useSelector, useDispatch } from 'react-redux';
 import { createOrgThunk } from '../../store/database/databaseThunk';
 import { toast } from 'react-toastify';
+
 export default function WorkspaceCombined() {
-const[tabIndex,setTabIndex]=useState(0);
+  const [tabIndex, setTabIndex] = useState(0);
   const alldbs = useSelector((state) => selectOrgandDb(state)) || [];
-  const [addedelement,setAddedelement]=useState(null);
+  const [addedelement, setAddedelement] = useState(null);
   const dispatch = useDispatch();
   const [org, setOrg] = useState();
   const [open, setOpen] = useState(false);
+
   const handleOpen = () => setOpen(true);
-  
 
   const sortAndRenderOrgList = () => {
     return Object.entries(alldbs)
@@ -28,7 +29,7 @@ const[tabIndex,setTabIndex]=useState(0);
             return -1;
           }
         }
-        
+
         const nameA = dbsA[0].org_id.name?.toUpperCase();
         const nameB = dbsB[0].org_id.name?.toUpperCase();
         if (nameA < nameB) {
@@ -40,9 +41,22 @@ const[tabIndex,setTabIndex]=useState(0);
         }
       })
       .map(([orgId, dbs], index) => {
+        const sortedDbs = [...dbs].sort((dbA, dbB) => {
+          // Compare and sort databases within an organization based on a property (e.g., name)
+          const nameA = dbA.name?.toUpperCase();
+          const nameB = dbB.name?.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          } else if (nameA > nameB) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+
         return (
           <Box key={orgId}>
-            <OrgList orgId={orgId} tabIndex={tabIndex} setTabIndex={setTabIndex} index={index} dbs={dbs} />
+            <OrgList orgId={orgId} tabIndex={tabIndex} setTabIndex={setTabIndex} index={index} dbs={sortedDbs} />
           </Box>
         );
       });
@@ -50,19 +64,26 @@ const[tabIndex,setTabIndex]=useState(0);
 
   const saveOrgToDB = async () => {
     const userid = localStorage.getItem("userid");
-    dispatch(createOrgThunk({ name: org, user_id: userid })).then((e)=>{
+    dispatch(createOrgThunk({ name: org, user_id: userid })).then((e) => {
       toast.success('Organisation created successfully!');
       setAddedelement(e.payload.data.org_id._id);
     });
     setOpen(false);
   };
+
   return (
     <>
       <Box>
         <Box sx={{ display: 'flex', m: 3 }}>
           <Button onClick={handleOpen} variant="contained">Create Organisation</Button>
-          <PopupModal title="create organisation" label="Organization Name" open={open} setOpen={setOpen}
-            submitData={saveOrgToDB} setVariable={setOrg} joiMessage={"Organization name"}
+          <PopupModal
+            title="create organisation"
+            label="Organization Name"
+            open={open}
+            setOpen={setOpen}
+            submitData={saveOrgToDB}
+            setVariable={setOrg}
+            joiMessage={"Organization name"}
           />
         </Box>
         <Box>
@@ -72,6 +93,7 @@ const[tabIndex,setTabIndex]=useState(0);
     </>
   );
 }
+
 WorkspaceCombined.propTypes = {
   dbs: PropTypes.string
-}
+};
