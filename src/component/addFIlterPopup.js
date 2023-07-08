d7875b1f5b71d0b0f7c4e87bf855b6afe524674a
Anushka Tomar <anushka.s.tomar21@gmail.com>
2023-07-08 14:18:20 +0530

    New Filter Model

--- src/component/addFIlterPopup.js
+++ src/component/addFIlterPopup.js
@@ -1,47 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import Modal from "@mui/material/Modal";
 import PropTypes from "prop-types";
-import Joi from "joi"
-// import CustomAutoSuggest from "./customAutoSuggest/customAutoSuggest";
-import { useValidator } from "react-joi"
-import {  TextField } from '@mui/material';
-import {  updateQuery } from "../api/filterApi"
+import Joi from "joi";
+import { useValidator } from "react-joi";
+import { TextField } from "@mui/material";
+import { updateQuery } from "../api/filterApi";
 import { getAllTableInfo } from "../store/allTable/allTableSelector";
 import { useDispatch, useSelector } from "react-redux";
 import { setAllTablesData } from "../store/allTable/allTableSlice";
-// import { useNavigate } from "react-router-dom";
 import CustomAutoSuggest from "./customAutoSuggest/customAutoSuggest";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "600px",
+  maxHeight: "calc(100vh - 100px)", // Set the maximum height of the popup
+  overflowY: "auto", // Add scroll when content overflows
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  p:4  
 };
 
-
-
 export default function AddFilterPopup(props) {
-  // const navigate = useNavigate();
   const AllTableInfo = useSelector((state) => getAllTableInfo(state));
-  const handleClose = () => {
-    props?.setEdit(false)
-    props.setOpen(false);
-  }
-
-  const [filterName, setFilterName] = useState('');
-  const [html, setHtml] = useState('');
-  const [text, setText] = useState('');
-  const dispatch = useDispatch();
+  const [filterName, setFilterName] = useState("");
+  const [html, setHtml] = useState("");
+  const [text, setText] = useState("");
   const [fields, setFields] = useState([]);
-  const [defaultValue,setDefaultValue]=useState(AllTableInfo?.tables[props?.tableName]?.filters[props?.filterId]?.htmlToShow || "");
+  const [aiQuery, setAiQuery] = useState("");
+  const [defaultValue, setDefaultValue] = useState(
+    AllTableInfo?.tables[props?.tableName]?.filters[props?.filterId]?.htmlToShow || ""
+  );
+  const textFieldRef = useRef(null);
+  const buttonContainerRef = useRef(null);
+  const dispatch = useDispatch();
+  const [textFieldHeight, setTextFieldHeight] = useState(null);
+
+  const handleClose = () => {
+    props?.setEdit(false);
+    props.setOpen(false);
+  };
+
+  console.log(aiQuery);
+
   const tableData = async () => {
-    const myObj =AllTableInfo?.tables[props?.tableName]?.fields;
+    const myObj = AllTableInfo?.tables[props?.tableName]?.fields;
     const arr = Object.keys(myObj).map((key) => ({
       name: myObj[key].fieldName,
       content: key,
@@ -52,8 +61,8 @@ export default function AddFilterPopup(props) {
   useEffect(() => {
     tableData();
   }, [props.tableName]);
-  const { state, setData, setExplicitField, validate } = useValidator({
 
+  const { state } = useValidator({
     initialData: {
       filterName: null,
     },
@@ -66,108 +75,93 @@ export default function AddFilterPopup(props) {
     validationOptions: {
       abortEarly: true,
     },
-  })
-
-  const createFilterJoi = (e) => {
-
-    e.persist();
-    const value = e.target.value;
-    setFilterName(value);
-
-    setData((old) => ({
-      ...old,
-      filterName: value,
-    }));
-    validate();
-  };
-
+  });
 
   useEffect(() => {
-    if (props?.edit == true) {
+    if (props?.edit === true) {
       const editDataValues = AllTableInfo?.tables[props?.tableName]?.filters[props?.filterId]?.query;
-      const htmlToShow=AllTableInfo?.tables[props?.tableName]?.filters[props?.filterId]?.htmlToShow;
+      const htmlToShow = AllTableInfo?.tables[props?.tableName]?.filters[props?.filterId]?.htmlToShow;
       setDefaultValue(htmlToShow);
       const searchString = "where";
-      // let valueAfterWhere = ""
-      if(editDataValues.includes(searchString)){
-        const index = editDataValues.indexOf(searchString); 
+      if (editDataValues.includes(searchString)) {
+        const index = editDataValues.indexOf(searchString);
         if (index !== -1) {
-        //  valueAfterWhere =
           editDataValues.substring(index + searchString.length).trim();
-        // setQuery(valueAfterWhere)
+        }
       }
-      } else{
-        // setQuery("")
-      }     
-      setFilterName(AllTableInfo.tables[props?.tableName].filters[props?.filterId].filterName)
-      
+      setFilterName(AllTableInfo.tables[props?.tableName].filters[props?.filterId].filterName);
     }
-    // tableData();
-  }, [props])
+  }, [props]);
 
-
-  const updateFilter =async()=>{
-    var queryToSend = " ";
-    if (props?.dbData?.db?.tables[props?.tableName]?.view &&
-      Object.values(props?.dbData?.db?.tables[props?.tableName]?.view?.fields).length >= 1) {
-      const viewId = props?.dbData?.db?.tables[props?.tableName]?.view?.id
-      queryToSend = "select * from " + viewId + " where "+ text.trim();
+  const updateFilter = async () => {
+    let queryToSend = " ";
+    if (
+      props?.dbData?.db?.tables[props?.tableName]?.view &&
+      Object.values(props?.dbData?.db?.tables[props?.tableName]?.view?.fields).length >= 1
+    ) {
+      const viewId = props?.dbData?.db?.tables[props?.tableName]?.view?.id;
+      queryToSend = "select * from " + viewId + " where " + text.trim();
     } else {
-      queryToSend = "select * from " + props?.tableName + " where "+ text.trim();
+      queryToSend = "select * from " + props?.tableName + " where " + text.trim();
     }
     return queryToSend;
-  }
-
-  
-  
-  // const getQueryData = async () => {
-  //  const data =  await updateFilter();
-  //   let dataa = {
-  //     filterName: filterName,
-  //     query: data,
-  //     htmlToShow:html || ""
-
-      
-  //   }
-  //   const filter = await createFilter(props?.dbId, props?.tableName, dataa)
-  //   const filters = filter?.data?.data?.data?.tables[props?.tableName]?.filters;
-  //   const filterKey = Object.keys(filters).find(key => filters[key].filterName === filterName);
-  //     dispatch(setAllTablesData(
-  //     {
-  //       "dbId": props?.dbId,
-  //       "tables": filter.data.data.data.tables
-  //     }
-  //   ))
-  //   props?.setUnderLine(filterKey)
-  //   navigate(`/db/${props?.dbId}/table/${props?.tableName}/filter/${filterKey}`);
-  //   return dataa;
-  // }
+  };
 
   const editQueryData = async () => {
-   const data = await updateFilter();
+    const data = await updateFilter();
     const dataa = {
       filterId: props?.filterId,
       filterName: filterName,
       query: data,
-      htmlToShow:html
-    }
-    const updatedFilter = await updateQuery(props?.dbId, props?.tableName, dataa)
-    dispatch(setAllTablesData(
-      {
-        "dbId": props?.dbId,
-        "tables": updatedFilter.data.data.tables
-      }
-    ))
-  }
+      htmlToShow: html,
+    };
+    const updatedFilter = await updateQuery(props?.dbId, props?.tableName, dataa);
+    dispatch(
+      setAllTablesData({
+        dbId: props?.dbId,
+        tables: updatedFilter.data.data.tables,
+      })
+    );
+  };
+
   const handleTextChange = (text, html) => {
     setText(text.trim());
     setHtml(html);
-    setText(text);
+    adjustTextFieldHeight();
   };
-  
-  return (
-    <Box >
 
+  const adjustTextFieldHeight = () => {
+    const textField = textFieldRef.current;
+    if (textField) {
+      textField.style.height = "auto";
+      textField.style.height = textField.scrollHeight + "px";
+      setTextFieldHeight(textField.scrollHeight);
+    }
+  };
+
+  useEffect(() => {
+    updateButtonContainerPosition();
+  }, [textFieldHeight]);
+
+  const updateButtonContainerPosition = () => {
+    const textField = textFieldRef.current;
+    const buttonContainer = buttonContainerRef.current;
+    if (textField && buttonContainer) {
+      const textFieldRect = textField.getBoundingClientRect();
+      const buttonContainerRect = buttonContainer.getBoundingClientRect();
+      if (textFieldRect.bottom > buttonContainerRect.top) {
+        buttonContainer.style.position = "relative";
+        buttonContainer.style.marginTop = "10px";
+      } else {
+        buttonContainer.style.position = "absolute";
+        buttonContainer.style.top = `${textFieldRect.bottom}px`;
+        buttonContainer.style.marginTop = "0";
+      }
+    }
+  };
+
+  return (
+    <Box>
       <Modal
         disableRestoreFocus
         open={props.open}
@@ -175,77 +169,81 @@ export default function AddFilterPopup(props) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 2 }}>
-            <Typography id="title" variant="h6" component="h2" fontWeight="bold" fontSize={26}>
-              Update View
+        <Box sx={style} >
+          <Box sx={{ display: "flex", mb: 2, backgroundColor: "#E6E6E6", height: 50 }}>
+            <Typography id="modal-modal-title" variant="h6" component="h2" fontWeight="bold" fontSize={26} padding={1}>
+              Filter
             </Typography>
+            <IconButton style={{ marginLeft: "auto" }} onClick={handleClose} aria-label="Close">
+              <CloseIcon />
+            </IconButton>
           </Box>
 
           <Box style={{ display: "flex", flexDirection: "column" }}>
-
-            <Box sx={{ mb: 2, display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
-            <Typography sx={{ mt: 2, mr: 1, color: 'blue' }}>FILTER NAME</Typography>
-              <TextField error={
-                state?.$errors?.filterName.length === 0
-                  ? false
-                  : state.$errors?.filterName
-                    ? true
-                    : false
-              } autoFocus sx={{ width: 150, height: 60, fontWeight: 'bold' }} value={filterName} type="text" placeholder="enter filter name" onChange={(e) => { setFilterName(e.target.value); createFilterJoi(e); }} onBlur={() => setExplicitField("filterName", true)} />
-            </Box>
-            <div style={{ color: "red", fontSize: "12px", paddingLeft: "172px" }}>
-              {state.$errors?.filterName?.map((data) => data.$message).join(",")}
-            </div>
-
-            <Box sx={{display:'flex',flexDirection:'row',mb:2}}><Typography sx={{ mt: 2, mr: 1, color: 'blue' }}>WHERE</Typography>
-        {/* <TextField value={query} onChange={(e)=>{setQuery(e.target.value)}} placeholder="Enter the conditions"></TextField> */}
-        <div style={{marginLeft:'7.5%',paddingRight:'1%'}}>
-        <CustomAutoSuggest
-          getInputValueWithContext={handleTextChange}
-          width="400px"
-          suggestion={fields}
-          setHtml={setHtml}
-          setText={setText}
-          defaultValue={defaultValue}
-        />
-        </div>
-</Box>
-          </Box>
-
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-          
-            <Box>
-              {props?.edit == true && <Button className="mui-button" onClick={() => {
-                editQueryData()
-                handleClose()
-              }} variant="contained">Edit</Button>}
-            </Box>
-
-            <Box>
-              <Button variant="outlined" className="mui-button-outlined" onClick={handleClose}>
-                Cancel
-              </Button>
-            </Box>
-
-          </Box>
-        </Box>
-
-      </Modal>
+  <Box sx={{ mb: 2, display: "flex", alignItems: "center" }}>
+    <Box style={{ flex: 1 }}>
+      <TextField
+        ref={textFieldRef}
+        placeholder="Example : Column 1 contain USA"
+        style={{ width: "599px" }}
+        multiline
+        onChange={(e) => setAiQuery(e.target.value)}
+      />
     </Box>
-  );
+   
+  </Box>
+  <Button
+      className="mui-button"
+      style={{ width: "180px", height: "40px", fontSize: "12px", marginLeft: "420px" }}
+      ref={buttonContainerRef}
+    >
+      Generate Query by AI
+    </Button>
+<br/>
+  <div style={{ color: "red", fontSize: "12px", paddingLeft: "172px" }}>
+    {state.$errors?.filterName?.map((data) => data.$message).join(",")}
+  </div>
+
+  <Box sx={{  flexDirection: "row", mb: 2 }}>
+    <div style={{ paddingRight: "1%", width: "100%",margintop:"90px" }}>
+      <CustomAutoSuggest
+        getInputValueWithContext={handleTextChange}
+        width="593px"
+        suggestion={fields}
+        setHtml={setHtml}
+        setText={setText}
+        defaultValue={defaultValue}
+      />
+    </div>
+  </Box>
+</Box>
+
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button
+            className="mui-button"
+            onClick={() => {
+              editQueryData();
+              handleClose();
+            }}
+            variant="contained"
+          >
+            Save
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  </Box>
+);
 }
 
 AddFilterPopup.propTypes = {
-  open: PropTypes.bool,
-  setOpen: PropTypes.func,
-  dbId: PropTypes.any,
-  tableName: PropTypes.any,
-  edit: PropTypes.any,
-  filterId: PropTypes.any,
-  dbData: PropTypes.any,
-  setEdit: PropTypes.func,
-  setUnderLine:PropTypes.any
+open: PropTypes.bool,
+setOpen: PropTypes.func,
+dbId: PropTypes.any,
+tableName: PropTypes.any,
+edit: PropTypes.any,
+filterId: PropTypes.any,
+dbData: PropTypes.any,
+setEdit: PropTypes.func,
+setUnderLine: PropTypes.any,
 };
-
