121f021fcd3d9a86e3db36d49135f746b803f508
ShoaibMansoori <shoaibmansuri556@gmail.com>
2023-07-21 16:01:10 +0530

    done user ts

---
+++ src/store/user/userThunk.ts
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { add , remove} from "./userSlice";
+import { getCurrentUser } from "../../api/userApi";
+import { AddPayloadType } from "../../types/userDataType";
+
+
+export const saveUser = createAsyncThunk (
+    "user/saveUser", async (payload , {dispatch}) =>{
+        const data = await getCurrentUser();
+        const response:AddPayloadType=data.data.data;
+        dispatch(add(response));
+        return 5;
+    }
+);
+
+
+export const removeUser = createAsyncThunk (
+    "user/removeUser", async (_payload , {dispatch}) =>{
+        dispatch(remove);
+        return 5;
+    }
+);
