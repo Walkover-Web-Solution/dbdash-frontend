f06bea950eb6e9eca8835963309064f657569f6e
gourav222 <gouravparmar775566@gmail.com>
2023-07-22 10:38:26 +0530

    Table Redcuer Js to Ts Conversion

--- src/store/table/tableSelector.js
+++
@@ -1,6 +0,0 @@
-export const getTableInfo=(state)=>{
-    const {table}=state;
-    console.log("table",table);
-    const {columns,data,skipReset,tableId,dbId,pageNo,isMoreData,filterId}=table;
-    return {columns,data, skipReset,tableId,dbId,pageNo,isMoreData,filterId};
-}
